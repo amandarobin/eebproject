1f7a09354ec0c6f24f76180828288ec43a1da63b
amandarobin <32082400+amandarobin@users.noreply.github.com>
2017-09-24 10:39:34 -0700

    Section 9.6

--- Section9.6.R
+++ Section9.6.R
@@ -4,3 +4,18 @@ hur <- read.csv(hur.url,comment.char='#')
 hur
 print(hur)
 str(hur)
+# Class types of each variable:
+#Year   = integer
+#Name  = Factor                   
+#MasFem     = mumber             
+#MinPressure_before   = integer
+#Minpressure_Updated.2014= integer
+#Gender_MF    = integer           
+#Category = integer              
+#alldeaths = integer             
+#NDAM   = integer                 
+#Elapsed.Yrs = integer             
+#Source = factor
+#ZMasFem = number
+#ZMinPressure_A = number
+#ZNDAM = number
