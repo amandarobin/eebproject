1d956aa1b2b27dee7d492ea530c9165b1692484f
amandarobin <32082400+amandarobin@users.noreply.github.com>
2017-09-25 12:12:45 -0700

    Chapter 11

--- Chapter11.R
+++ Chapter11.R
@@ -1,8 +1,6 @@
-# Section 11 
-
+# Chapter 11
 course.url <- "http://kingaa.github.io/R_Tutorial/"
 download.file(paste0(course.url,"Intro1.R"),destfile="Intro1.R",mode="w")
-
 
 light <- c(20,20,20,20,21,24,44,60,90,94,101)
 rmax <- c(1.73,1.65,2.02,1.89,2.61,1.36,2.37,2.08,2.69,2.32,3.67)
@@ -14,38 +12,27 @@ abline(fit)
 
 source("Intro1.R")
 
+download.file(paste0(course.url,"Intro2.R"),destfile="Intro2.R",mode="w")
+download.file(paste0(course.url,"ChlorellaGrowth.csv"),destfile="ChlorellaGrowth.csv",mode="w")
 
-X <- read.csv('ChlorellaGrowth.csv',comment.char='#')
-# using '#' as a comment character allows us the CSV file to be self-documenting
-    #Unmod
+X <- read.csv("ChlorellaGrowth.csv",comment.char='#')
+
+Light <- X[,1]
+rmax <- X[,2]
+
+source("MODlinregIntro2.R") #updated modification using log of light
+
+modplot # this is the call of the modified graph of log of light intensity 
+
+plot(fit)
+??plot.lm # describes how plot.lm will create a series of exploratory plots for the linear model regardless of the model's suitablity for the data
+
+#growth rate vs. light intensity with x axis from 0 to 120, y axis from 1 to 4
 par(cex=1.5,cex.main=0.9)
 plot(rmax~light, xlim=c(0,120),ylim=c(1,4), data=X,xlab="light intensity (uE/m2/s)",ylab="maximum growth rate (1/d)",pch=16) 
-# xlab and ylab are x and y axis labels, pch is "plotting character"
-# cex is 'character expansion' - cex=1.5 increases symbol & label sizes by 50%
-# cex.main sets the character expansion for the main title of the plot 
-
 title(main="Data from Fussmann et al. (2000) system")
-fit <- lm(rmax~Light)
-summary(fit); abline(fit) 
-c1 <- round(fit$coef[1],digits=3) 	
-c2 <- round(fit$coef[2],digits=3) 
-text(50,3,paste("rmax=",c1,"+",c2,"light")) 
 
-  #Mod version
-LogLight=log(Light)
-par(cex=1.5,cex.main=0.9)
-plot(rmax~LogLight, xlim=c(0,120),ylim=c(1,4), data=X,xlab="light intensity (uE/m2/s)",ylab="maximum growth rate (1/d)",pch=16) 
-# xlab and ylab are x and y axis labels, pch is "plotting character"
-# cex is 'character expansion' - cex=1.5 increases symbol & label sizes by 50%
-# cex.main sets the character expansion for the main title of the plot 
-
-title(main="Data from Fussmann et al. (2000) system")
-fit <- lm(rmax~LogLight)
-summary(fit); abline(fit) 
-c1 <- round(fit$coef[1],digits=3) 	
-c2 <- round(fit$coef[2],digits=3) 
-text(50,3,paste("rmax=",c1,"+",c2,"LogLight")) 
-
+# side by side graphs:
 Logrmx=log(rmax)
 LogLight=log(Light)
 par(cex=1.5,cex.main=0.9)
@@ -78,7 +65,7 @@ c2 <- round(fit$coef[2],digits=3)
 text(50,3,paste("Logrmax=",c1,"+",c2,"LogLight")) 
 
 
-
+# Last Exercise 
 x <- c(1:10)
 y <- (5*x)+3 
 y
